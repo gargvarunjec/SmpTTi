9bb08777993a461d83fc5791d60c9a902412d92a
gargvarunjec <gargvarunjec@gmail.com>
2023-10-21 17:31:17 +0530

    Created Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {BrowserRouter, Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import SignOut from './pages/SignOut';
 
 function App() {
   return (
-    <div className='bg-red-300'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/About' element={<About/>} />
+        <Route path='/Profile' element={<Profile/>} />
+        <Route path='/SignIn' element={<Signin/>} />
+        <Route path='/SignOut' element={<SignOut/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 

ed34c510796549e0614dfde16482bfa411bccf15
gargvarunjec <gargvarunjec@gmail.com>
2023-11-04 18:25:44 +0530

    created signup api

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO)
@@ -21,3 +23,4 @@ app.listen(3000, () => {
 
 
 app.use('/api',userRouter);
+app.use('/api/auth',authRouter);

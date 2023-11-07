ff22c9601c90a14c145e81d5fb1473db5aa6823a
gargvarunjec <gargvarunjec@gmail.com>
2023-11-07 18:58:04 +0530

    created signin api

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signIn, signup } from '../controllers/auth.controller.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/signup',signup);
-
+authRouter.post('/signin',signIn)
 export default authRouter;

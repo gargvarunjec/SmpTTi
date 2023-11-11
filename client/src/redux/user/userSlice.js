ba075c723a9f66c07417d450aaa71a19a1659bc7
gargvarunjec <gargvarunjec@gmail.com>
2023-11-11 14:20:15 +0530

    update user functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,30 +1,48 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-      currentUser:null,
-      error:null,
-      loading:false
-}
+  currentUser: null,
+  error: null,
+  loading: false,
+};
 
 const userSlice = createSlice({
-      name:'user',
-      initialState,
-      reducers:{
-            signInStart: (state) => {
-                  state.loading = true;
-            },
-            signInSuccess: (state,action) => {
-                  state.loading = false;
-                  state.error=null;
-                  state.currentUser = action.payload;
-            },
-            signInFailure: (state,action) => {
-                  state.loading = false;
-                  state.error = action.payload;
-            }
-      }
-
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+    },
+    signInSuccess: (state, action) => {
+      state.loading = false;
+      state.error = null;
+      state.currentUser = action.payload;
+    },
+    signInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.loading = false;
+      state.error = null;
+      state.currentUser = action.payload;
+    },
+    updateUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
 });
 
-export const { signInFailure, signInStart , signInSuccess } = userSlice.actions;
+export const {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+} = userSlice.actions;
 export default userSlice.reducer;

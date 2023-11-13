805081be6a43dcb0cca8b37b32af7b860eb26396
gargvarunjec <gargvarunjec@gmail.com>
2023-11-13 22:53:48 +0530

    create getuersListing api route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getListing,
   test,
   updateUser,
 } from "../controllers/user.controller.js";
@@ -11,4 +12,5 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verfiyUser, updateUser);
 router.delete("/delete/:id", verfiyUser, deleteUser);
+router.get("/listings/:id", verfiyUser, getListing);
 export default router;

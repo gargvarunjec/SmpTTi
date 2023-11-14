f0512c118309a1bb4bf3f68bd048083ac5f4fdb7
gargvarunjec <gargvarunjec@gmail.com>
2023-11-14 16:52:13 +0530

    complete yourUser Listing ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import {
   getDownloadURL,
   getStorage,
@@ -27,6 +27,8 @@ export default function Profile() {
   const [fileError, setFileError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingError, setShowListingError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const fileRef = useRef(null);
 
   const dispatch = useDispatch();
@@ -120,6 +122,21 @@ export default function Profile() {
       dispatch(signOutFailure(error.message));
     }
   };
+
+  const handleShowListing = async () => {
+    try {
+      setShowListingError(false);
+      const res = await fetch(`api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingError(true);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -179,7 +196,13 @@ export default function Profile() {
         >
           {loading ? "loading..." : "Update"}
         </button>
-        <Link to={"/create-listing"} className="text-lg bg-green-700 p-3 rounded-lg text-center text-white hover:opacity-95"> Create Listing</Link>
+        <Link
+          to={"/create-listing"}
+          className="text-lg bg-green-700 p-3 rounded-lg text-center text-white hover:opacity-95"
+        >
+          {" "}
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
@@ -199,6 +222,44 @@ export default function Profile() {
         {" "}
         {updateSuccess ? "User Updated Successfully " : ""}{" "}
       </p>
+      <button onClick={handleShowListing} className="text-green-700 w-full">
+        {" "}
+        Show Listings{" "}
+      </button>
+      <p className="text-red-700 mt-5">
+        {" "}
+        {showListingError ? "Error showing Listing" : " "}{" "}
+      </p>
+      {userListings &&
+        userListings.length > 0 &&
+        <div className="flex flex-col gap-4">
+          <h1 className="text-2xl font-semibold text-center mt-7"> Your Listings </h1>
+        {userListings.map((listing) => (
+          <div
+            key={listing._id}
+            className="flex justify-between border rounded-lg  p-3 items-center gap-4"
+          >
+            <Link to={`/listing/${listing._id}`}>
+              <img
+                src={listing.imageUrls[0]}
+                alt="listing cover"
+                className="h-16 w-16 object-contain"
+              />
+            </Link>
+              <Link className="text-slate-700 font-semibold truncate hover:underline flex-1" to={`/listing/${listing._id}`}>
+                <p >
+                  {" "}
+                  {listing.name}{" "}
+                </p>
+              </Link>
+
+              <div className="flex flex-col">
+                <button className="text-green-700"> Edit </button>
+                <button className="text-red-700"> Delete </button>
+              </div>
+          </div>
+        ))}
+        </div>}
     </div>
   );
 }

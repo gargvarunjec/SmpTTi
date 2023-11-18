90854ea6bc495846456a537dbb56b2aca02345de
gargvarunjec <gargvarunjec@gmail.com>
2023-11-18 13:57:33 +0530

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold text-slate-800 mb-8"> About SmpTTi </h1>
+      <p className="mb-4 text-slate-700 ">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+        deleniti assumenda consequatur labore numquam. Provident suscipit esse
+        assumenda error eveniet placeat dolore, odio laborum corporis quia hic
+        sapiente ducimus magnam?
+      </p>
+      <p className="mb-4 text-slate-700 ">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+        deleniti assumenda consequatur labore numquam. Provident suscipit esse
+        assumenda error eveniet placeat dolore, odio laborum corporis quia hic
+        sapiente ducimus magnam?
+      </p>
+      <p className="mb-4 text-slate-700 ">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+        deleniti assumenda consequatur labore numquam. Provident suscipit esse
+        assumenda error eveniet placeat dolore, odio laborum corporis quia hic
+        sapiente ducimus magnam?
+      </p>
+    </div>
+  );
 }

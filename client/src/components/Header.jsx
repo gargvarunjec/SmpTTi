43ac55557c91f4c7e57b11c6bcdae251ad3beed5
gargvarunjec <gargvarunjec@gmail.com>
2023-11-19 00:19:35 +0530

    Corrected hidden Signin button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,7 +25,7 @@ export default function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <h1 className="font-bold text-sm sm:text-xl">
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
           <Link to="/">
             <span className="text-slate-500">Smp</span>
             <span className="text-slate-700">TT</span>
@@ -47,7 +47,7 @@ export default function Header() {
             <FaSearch className="text-slate-600" />
           </button>
         </form>
-        <ul className="flex gap-3">
+        <ul className="flex gap-4">
           <Link to="/">
             <li className="hidden sm:inline hover:underline text-slate-700">
               Home
@@ -66,9 +66,7 @@ export default function Header() {
                 alt="Profile"
               />
             ) : (
-              <li className="hidden sm:inline hover:underline text-slate-700">
-                Sign in
-              </li>
+              <li className="hover:underline text-slate-700">Sign in</li>
             )}
           </Link>
         </ul>

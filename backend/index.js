80d67c2318433044790d93c5b63834072b571d8b
gargvarunjec <gargvarunjec@gmail.com>
2023-11-18 15:06:52 +0530

    add preinstallation

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,9 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import listingRouter from "./routes/listing.routes.js";
-
+import path from "path";
 dotenv.config();
 
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -19,6 +16,11 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 app.listen(3000, () => {
   console.log("Server is listening");
 });
@@ -26,6 +28,11 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
